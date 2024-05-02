1b6a3d2ec1997b0da75e22fd7c7cf4d531399758
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2024-05-02 15:05:06 +0200

    TLG-001 : TMB : add : created folders structure

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  // Definisci le tue props qui
+}
+
+const Button: React.FC<Props> = ({ /* props */ }) => {
+  return (
+    <div>
+      {/* Contenuto del componente */}
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/redux/store.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  // Definisci le tue props qui
+}
+
+const ComponentName: React.FC<Props> = ({ /* props */ }) => {
+  return (
+    <div>
+      {/* Contenuto del componente */}
+    </div>
+  );
+};
+
+export default ComponentName;
