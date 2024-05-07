d2d26a33d573c4c82f53a906de89aaa433b69e59
DavidePiccica <139143867+DavidePiccica@users.noreply.github.com>
2024-05-07 14:29:35 +0200

    added redux/dotenv/contentful/sass

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+
+const Navbar=() => {
+    return (
+        <>
+           <nav>
+            <img alt="the modern botique logo"></img>
+                <div className="navbarTitleName">
+                    <h1>TMB</h1>
+                    <h3> The modern botique</h3>
+                </div>
+                <div className="navbarMenuItem">
+                    <ul>
+                        
+                    </ul>
+                </div>
+           </nav>
+
+        </>
+    )
+}
+
+export default Navbar
