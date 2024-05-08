bdba91f76c938b34740256cbe557618b018a5e29
Emilio Lenglet <lemigliot@gmail.com>
2024-05-08 15:02:21 +0200

    TLG:005:TMB:ADD:creati bottoni, regolare solo grandezza font

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 // In App.tsx
 import React from "react";
-import "./App.css";
 import Buttontmg from "./components/Buttons/ButtonTmg";
 import Buttontmg2 from "./components/Buttons/ButtonTmg2";
 import Buttontmg3 from "./components/Buttons/ButtonTmg3";
 
 const App: React.FC = () => {
   const handleClick = () => {
-    console.log("ciao");
+    console.log("BUTTON 1");
   };
   const handleClick2 = () => {
-    console.log("ciao2");
+    console.log("BUTTON 2");
   };
-  const handleAddToCart = () => {
-    console.log("handle to cart");
+  const handleClick3 = () => {
+    console.log("BUTTON 3");
   };
 
   return (
     <>
-      <Buttontmg onClick={handleAddToCart} label="BUTTON 1" />
+      <Buttontmg onClick={handleClick} label="BUTTON 1" />
       <Buttontmg2 onClick={handleClick2} label="BUTTON 2" />
-      <Buttontmg3 onClick={handleClick} label="BUTTON 3" />
+      <Buttontmg3 onClick={handleClick3} label="BUTTON 3" />
     </>
   );
 };
