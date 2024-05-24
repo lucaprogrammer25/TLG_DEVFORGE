c71e6f71d021ba870684d413906f367e2c32fa7c
DavidePiccica <139143867+DavidePiccica@users.noreply.github.com>
2024-05-24 17:59:04 +0200

    TLG-042:TMBADD:added on first mount effect

--- src/pages/LandingPage/HeroSection.tsx
+++ src/pages/LandingPage/HeroSection.tsx
@@ -1,34 +1,45 @@
- import { useTypeSelector ,useTypeDispatch} from "../../redux/typeHooks"
+import { useTypeSelector, useTypeDispatch } from "../../redux/typeHooks";
 import fetchDataContentful from "../../redux/fetch/fetchContentful";
-import { useEffect } from "react"; 
+import { useEffect, useState } from "react";
 import CarouselPage from "./CarouselPage";
 import Content from "./Content";
- import AnimatedComponent from "./AnimatedCard"; 
-
+import AnimatedComponent from "./AnimatedCard";
 
 const HeroSection = () => {
-  const { data } = useTypeSelector((state) => state.contentful)
+  const { data } = useTypeSelector((state) => state.contentful);
   const dispatch = useTypeDispatch();
-  
-  const landingImage =  data.items && data.items[6]?.fields.bannerImage.fields.file.url;
-  
-  
+  const [loaded, setLoaded] = useState(false);
+
+  const landingImage = data.items && data.items[6]?.fields.bannerImage.fields.file.url;
 
   useEffect(() => {
-    dispatch(fetchDataContentful()) 
-  },[dispatch]) 
-  
-  return (
-    <>
-        <div className="heroSectionContainer">
-           {/* <img  src={landingImage} className="heroSectionImage"alt="landing image"></img> */}
-           <video  className="heroSectionVideo"src={landingImage} autoPlay muted loop playsInline controls={false} disablePictureInPicture/> 
-          <CarouselPage/>
-          <Content/>
-          <AnimatedComponent/> 
-      </div>   
-    </>
-  )
-}
+    dispatch(fetchDataContentful());
+  }, [dispatch]);
 
-export default HeroSection
+  const handleVideoLoaded = () => {
+    setLoaded(true); 
+  };
+
+  return (
+    <div className="heroSectionContainer">
+      <div className={`heroSectionVideoContainer ${loaded ? "zoomOut" : ""}`}>
+        <video
+          className="heroSectionVideo"
+          src={landingImage}
+          autoPlay
+          muted
+          loop
+          playsInline
+          controls={false}
+          disablePictureInPicture
+          onLoadedData={handleVideoLoaded}
+        />
+      </div>
+      <CarouselPage />
+      <Content />
+      <AnimatedComponent />
+    </div>
+  );
+};
+
+export default HeroSection;
